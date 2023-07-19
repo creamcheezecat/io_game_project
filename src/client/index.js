36749df1300023fdd89438ef0164f22dbf51585d
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-07-19 13:25:07 +0900

    부트스트랩 사용 변경

--- src/client/index.js
+++ src/client/index.js
@@ -5,7 +5,10 @@ import { downloadAssets } from './assets';
 import { initState } from './state'
 import { setLeaderboardHidden } from './leaderboard';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// I'm using a tiny subset of Bootstrap here for convenience - there's some wasted CSS,
+// but not much. In general, you should be careful using Bootstrap because it makes it
+// easy to unnecessarily bloat your site.
+import './css/bootstrap-reboot.css';
 import './css/main.css';
 
 const playMenu = document.getElementById('play-menu');
